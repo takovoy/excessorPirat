770de4a9ada665abb9dcac1c7766679e26e4cf49
alexander.sustavov <alexander.sustavov@gotechsoftware.com>
2018-03-29 18:55:50 +0300

    append out files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var jsImport = require('gulp-js-import');
 var minify = require('gulp-minify');
+var rename = require('gulp-rename');
 
 gulp.task('compare',function () {
     gulp.src('src/index.js')
         .pipe(jsImport())
+        .pipe(rename('excessorPirat.js'))
         .pipe(minify())
         .pipe(gulp.dest('dest/'));
 });
